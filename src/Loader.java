877a3211f4c3609bb12f9dfa94978a08ce76afd1
Oleg Lymarev <oleg.lymareff@yandex.ru>
2021-09-08 18:23:41 +0300

    First commit

---
+++ src/Loader.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Loader {
+    public static void main(String[] args) {
+        int a,b;
+        int[][] ochki = {
+                {4, 3, 2, 5, 1},
+                {3, 4, 4, 6, 2},
+                {2, 5, 4, 3, 2},
+                {8, 4, 2, 0, 4},
+                {4, 4, 3, 2, 9},
+                {1, 1, 1, 1, 1}
+        };
+        String result = new String ("bob")
+    }
+}
